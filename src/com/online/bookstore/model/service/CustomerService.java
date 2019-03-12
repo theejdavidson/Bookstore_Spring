6f93412364dcdbc90aa42c3620bebc7873649d77
Ethan <theejdavidson@gmail.com>
2019-03-12 00:03:46 -0500

    Dependency injection with Spring- created a Payment interface and PaymentImpl class with Order accessing the interface instead of the logic-containing class

--- src/com/online/bookstore/model/service/CustomerService.java
+++ src/com/online/bookstore/model/service/CustomerService.java
@@ -4,6 +4,7 @@ package com.online.bookstore.model.service;
 import com.online.bookstore.model.customer.Customer;
 
 public class CustomerService {
+
 	private Customer customer;
 	
 	public void setCustomer(Customer customer) {
